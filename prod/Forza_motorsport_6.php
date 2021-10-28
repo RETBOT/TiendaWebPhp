1b2066f852fdbf3d5f5d8736c2f05a3b8172086b
RETBOT <robertoesquivelr@gmail.com>
2021-10-27 19:59:34 -0500

    cambio

--- prod/Forza_motorsport_6.php
+++ prod/Forza_motorsport_6.php
@@ -35,10 +35,14 @@ include '../global/conexion.php';
             <div class="juego__contenido">
                 <p>Forza Motorsport 6 es la acción incesante a velocidad de simulación en el juego de carreras más hermoso y completo de esta generación. Colecciona, personaliza y pilota más de 450 coches Forzavista™ con cabinas funcionales y daños realistas. Compite en carreras épicas para 24 jugadores en 26 emplazamientos de fama mundial. Domina el clima lluvioso y las carreras nocturnas en tu camino a la victoria.</p>
 
-                <form class="formulario">
-                    <input class="formulario__campo" type="number" placeholder="Cantidad" min="1" value="1"> 
-                    <p class="producto__precio">Precio $800.00</p>
-                    <input class="formulario__submit" type="submit" value="Agregar al Carrito">
+                <form method="post" class="formulario">
+                        <input type="hidden" id="Juego" name="Juego" value="Forza motorsport 6">
+                        <p class="producto__precio">Cantidad</p>
+                        <input type="number" id="Cantidad" name="Cantidad" class="formulario__campo"  placeholder="Cantidad" value="1"> 
+                        <input type="hidden" id="Precio" name="Precio" value="800.00">
+                        <p class="producto__precio">Precio $800.00</p>
+                        <input type="hidden" id="Imagen" name="Imagen" value="../img/5.jpg">
+                        <button class="formulario__submit" type="submit" value="Agregar">Agregar al carrito</button>
                 </form>
             </div>
         </div>
