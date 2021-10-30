3dff5abe91be72e92730171bc58d86f4d5ada8f0
RETBOT <robertoesquivelr@gmail.com>
2021-10-29 21:01:09 -0500

    mensajes

--- global/conexion.php
+++ global/conexion.php
@@ -8,7 +8,7 @@ try{
 //echo "<script>alert('Conectado... ')</script>";
 
 }catch(PDOException $e){
-    echo "<script>alert('Error... ')</script>";
+    //echo "<script>alert('Error... ')</script>";
 }
 
 ?>
