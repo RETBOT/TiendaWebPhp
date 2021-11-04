dccf664a8f512c5044eff4b68fe93900ab78760a
RETBOT <robertoesquivelr@gmail.com>
2021-11-04 16:32:52 -0600

    Recibo completado

--- carrito/Recibo.php
+++ carrito/Recibo.php
@@ -25,23 +25,6 @@ require_once '../libreria/dompdf/autoload.inc.php';
     $correo = $objRespuesta->payer->email_address;
     $nombre = $objRespuesta->payer->name->given_name;
     $apellido = $objRespuesta->payer->name->surname;
-
-    // informacion de los prouctos comprados por el usuario 
-    $sentenciaProductos = $pdo->prepare("SELECT * FROM 
-    tbldetalleventa, tblproductos 
-    WHERE tbldetalleventa.IDPRODUCTO=tblproductos.ID 
-    and tbldetalleventa.IDVENTA=:ID");
-    $sentenciaProductos->bindParam(":ID", $IDVENTA);
-    $sentenciaProductos->execute();
-    $listaProducto = $sentenciaProductos->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($_SESSION['CARRITO'] as $indice => $producto) {
-        $NombresJuegos = $listaProducto[$indice]['Nombre'];
-        $PrecioJuegos = $listaProducto[$indice]['PRECIOUNITARIO'];
-        $CantidadJuegos = $listaProducto[$indice]['CANTIDAD'];
-    //print_r($CantidadJuegos);
-    }
-    //print_r($listaProducto);
   ?>
     <html>
         <head>
@@ -54,9 +37,20 @@ require_once '../libreria/dompdf/autoload.inc.php';
         </head>
         <body>
             <h3 style="text-align: center;">Recibo de compra</h3>
-            <center>
+            <h3 style="text-align: center;">Informacion de usuario</h3>
+            <p>Nombre: <?php echo $nombre." ".$apellido ?></p>
+            <p>Numero de compra: <?php echo $ID ?> </p>
+            <p>Clave de transacción: <?php echo $ClaveTransaccion ?> </p>
+            <p>Estado de transacción: <?php echo $state ?> </p>
+            <p>Total de compra: <?php echo $total ?></p>
+            <p>Divisa: <?php echo $currency ?> </p>
+            <p>Correo electronico: <?php echo $correo ?></p>
+
+            <h3 style="text-align: center;">Productos</h3>
+    <center>
             <?php if(!empty( $_SESSION['CARRITO'])) { ?>
-        <table style="border-collapse: collapse; text-align: center;"  border="5"  bordercolor="#1FC52E">
+    <div class="producto">
+        <table style="border-collapse: collapse; text-align: center; width: 100%;"  border="5"  bordercolor="#1FC52E">
                     <tr>
                         <th width="10%">Imagen</th>
                         <th width="30%">Juego</th>
@@ -66,7 +60,7 @@ require_once '../libreria/dompdf/autoload.inc.php';
                     </tr>
                     <?php foreach($_SESSION['CARRITO'] as $indice=>$producto){ ?>
                     <tr>
-                        <td width="25%"><img class="producto__imagen" src="http://<?php echo $_SERVER['HTTP_HOST'].'/TiendaWebPhp/img/'.$producto['Imagen'] ?>" alt="imagen juego"></td>
+                        <td width="10%"><img width="100" height="150" class="producto__imagen" src="http://<?php echo $_SERVER['HTTP_HOST'].'/TiendaWebPhp/img/'.$producto['Imagen'] ?>" alt="imagen juego"></td>
                         <td width="30%"><?php echo $producto['Juego'] ?></td>
                         <td width="15%"><?php echo $producto['Cantidad'] ?></td>
                         <td width="20%"><?php echo '$'.$producto['Precio'] ?></td>
@@ -77,10 +71,10 @@ require_once '../libreria/dompdf/autoload.inc.php';
                     <?php } ?>
                     <tr>
                         <td colspan="4" align="right">Total</td>
-                        <td align="right"><?php echo '$'.number_format($total,2);?></td>
-                        <td></td>
+                        <td align="center"><?php echo '$'.number_format($total,2);?></td>
                     </tr>
-            </table></h3>
+            </table>
+    </div>
             <?php }?></center>
 
         </body>
@@ -102,5 +96,5 @@ require_once '../libreria/dompdf/autoload.inc.php';
     $dompdf->setPaper('letter');
     //$dompdf->setPaper('A4','landscope');
     $dompdf->render();
-    $dompdf->stream("recibo.pdf",array("Attachment" => false));
+    $dompdf->stream("recibo.pdf",array("Attachment" => true));
 ?>
